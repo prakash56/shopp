961b64b7ee8f1ea2ed6fa9e1b5c817d7b117a2e2
prakash56 <prakashgupta56@gmail.com>
2024-12-09 18:40:13 +0530

    api image

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.post('/upload', upload.single('product'), (req, res) => {
         }
         res.json({
             success: 1,
-            image_url: `http://localhost:${port}/images/${req.file.filename}`
+            image_url: `https://shopper-api-uqo8.onrender.com/images/${req.file.filename}`
         });
     } catch (error) {
         console.error('Error during file upload:', error);
